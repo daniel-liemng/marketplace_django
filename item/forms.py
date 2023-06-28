d57456ecce9a63d78b1f7106123cca2ecd7b7eb2
Daniel <daniel.liemng@gmail.com>
2023-06-28 18:50:34 -0400

    dashboard - item CRUD

--- item/forms.py
+++ item/forms.py
@@ -4,6 +4,7 @@ from .models import Item
 
 INPUT_CLASS = 'w-full px-4 py-2 rounded-xl border outline-teal-500'
 
+
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
@@ -12,6 +13,26 @@ class NewItemForm(forms.ModelForm):
             'category': forms.Select(attrs={
                 'class': INPUT_CLASS
             }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASS
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASS
+            }),
+            'price': forms.TextInput(attrs={
+                'class': INPUT_CLASS
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASS
+            }),
+        }
+
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'image', 'is_sold')
+        widgets = {
             'name': forms.TextInput(attrs={
                 'class': INPUT_CLASS
             }),
--- item/views.py
+++ item/views.py
@@ -3,14 +3,18 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
+
+
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
-    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
-    
+    related_items = Item.objects.filter(
+        category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+
     return render(request, 'item/detail.html', {'item': item, 'related_items': related_items})
+
 
 @login_required
 def new(request):
@@ -27,3 +31,28 @@ def new(request):
         form = NewItemForm()
 
     return render(request, 'item/form.html', {'form': form, 'title': 'New Item'})
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {'form': form, 'title': 'Edit Item'})
+
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
