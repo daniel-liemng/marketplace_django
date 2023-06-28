cdde96421813e3af243998e2e8c94b43644e5b61
Daniel <daniel.liemng@gmail.com>
2023-06-28 17:55:13 -0400

    add new item

--- core/forms.py
+++ core/forms.py
@@ -2,14 +2,16 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+INPUT_CLASS = 'w-full p-2 rounded-lg outline-teal-500'
+
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Your username',
-        'class': 'w-full p-2 rounded-lg outline-none'
+        'class': INPUT_CLASS
     }))  
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Your password',
-        'class': 'w-full p-2 rounded-lg outline-none'
+        'class': INPUT_CLASS
     }))
 
 class SignupForm(UserCreationForm):
@@ -19,17 +21,17 @@ class SignupForm(UserCreationForm):
 
     username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Your username',
-        'class': 'w-full p-2 rounded-lg outline-none'
+        'class': INPUT_CLASS
     }))
     email = forms.CharField(widget=forms.EmailInput(attrs={
         'placeholder': 'Your email',
-        'class': 'w-full p-2 rounded-lg outline-none'
+        'class': INPUT_CLASS
     }))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Your password',
-        'class': 'w-full p-2 rounded-lg outline-none'
+        'class': INPUT_CLASS
     }))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Your confirm password',
-        'class': 'w-full p-2 rounded-lg outline-none'
+        'class': INPUT_CLASS
     }))
